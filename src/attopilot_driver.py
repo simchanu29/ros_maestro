a3b2cbac5adcba844699bb963319aeefd3f0cb3e
HELIOS <helios@ensta-bretagne.org>
2018-02-14 19:18:35 +0100

    corrections erreurs attopilot drivers

--- src/attopilot_driver.py
+++ src/attopilot_driver.py
@@ -8,28 +8,30 @@ import rospy
 from std_msgs.msg import Float32
 
 def cb_voltage(msg):
+    global pub_volt
     # 255.75 pts sur 5V avec un ratio de 1:15.7
     coeffTheorique = 1/255.75*5.0*15.7
-    coeffAjustement = 0.92
-    pub_volt.publish(msg.data * coeffTheorique * coeffAjustement)
+    coeffAjustement = 1.0
+    pub_volt.publish(msg.data * coeffAjustement)
 
 def cb_current(msg):
+    global pub_curr
     # 255.75 pts sur 5V avec un ratio de 1:15.7
     coeffTheorique = 1/255.75*5.0*15.7
-    coeffAjustement = 0.92
-    pub_curr.publish(msg.data * coeffTheorique * coeffAjustement)
+    coeffAjustement = 1.0
+    pub_curr.publish(msg.data * coeffAjustement)
 
 if __name__ == '__main__':
 
     rospy.init_node('driver_attopilot')
-    rospy.loginfo("driver_maestro Node Initialised")
+    rospy.loginfo("Node Initialised")
 
     sub_voltage = ('~volt_topic', "battery1_voltage_raw")
     sub_current = ('~curr_topic', "battery1_current_raw")
 
     # Publication sans le suffixe _raw
-    rospy.Publisher(sub_voltage[:-4], Float32, maestro.cb_pwm)
-    rospy.Publisher(sub_current[:-4], Float32, maestro.cb_pwm)
+    rospy.Publisher(sub_voltage[:-4], Float32, queue_size=1)
+    rospy.Publisher(sub_current[:-4], Float32, queue_size=1)
 
     # Abonnenements
     pub_volt = rospy.Subscriber(sub_voltage, Float32, cb_voltage)
